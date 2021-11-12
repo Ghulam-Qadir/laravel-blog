f02c945524821d01b2f550270d77c26e589d48d0
Ghulam Qadir <68844510+Ghulam-Qadir@users.noreply.github.com>
2021-11-12 21:19:54 +0500

    new auth

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="{{ asset('storage/asset/js/jquery-3.5.1.slim.min.js') }}"></script>
+<script src=" {{ asset('storage/asset/js/bootstrap.min.js') }}"></script>
+</body>
+</html>
